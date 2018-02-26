9f60c628fb02e1e5462ab3e5273cea42f30a8ee5
Phil Hack <phil@philhack.com>
2018-02-26 16:46:39 +0000

    creating hand rolled jwt auth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,24 @@
 class ApiController < ApplicationController
   before_action :set_default_format
+  before_action authenticate_token!
 
   private
 
   def set_default_format
     request.format = :json
+  end
+
+  def authenticate_token!
+    JsonWebToken.decode(auth_token)
+    if payload.present?
+      @current_user = User.find(payload["sub"])
+      print @current_user
+    else
+      render json: {errors: ["Invalid auth token"]}, status: :unauthorized
+    end
+  end
+
+  def auth_token
+    @auth_token ||= request.headers.fetch(["Authorization"], "").split(" ").last
   end
 end
