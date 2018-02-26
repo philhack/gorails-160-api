444c7c195e56dbaa766254e623e3b1c6185b9149
Phil Hack <phil@philhack.com>
2018-02-26 13:15:51 +0000

    cleane up spacing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'user_token' => 'user_token#create'
+
       resources :locations do
         resources :recordings
 
@@ -10,6 +11,4 @@ Rails.application.routes.draw do
   end
 
   resources :locations
-
-
 end
