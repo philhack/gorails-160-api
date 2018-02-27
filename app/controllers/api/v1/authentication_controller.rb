4bf89d6b21d7931e9ea262f6035fb2dd111ac194
Phil Hack <phil@philhack.com>
2018-02-27 06:34:03 +0000

    finished hand rolled jwt

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,7 +1,12 @@
-class AuthenticationController < ApiController
-
+class Api::V1::AuthenticationController < ApiController
   skip_before_action :authenticate_token!
-  def create
 
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user.valid_password? params[:user][:password]
+      render json: {token: {sub: user.id}}
+    else
+      render json: { errors: ["Invalid email or password"]}
+    end
   end
 end
